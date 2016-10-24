5d215dd42b997fcf520b9c3c2785f6ceaf1dfd2e
aamulumi <florian@kauder.fr>
2016-10-25 00:02:20 +0200

    feat(server): Add GET /statistics/teams

--- server/tools/routes/statistics.js
+++ server/tools/routes/statistics.js
@@ -15,7 +15,24 @@ router.get('/users', (req, res) => {
             return Response.selectError(res, err);
         }
 
-        Response.success(res, 'Found', result);
+        Response.success(res, 'Statistics of users', {
+            users: result[0].users
+        });
     });
 });
+
+router.get('/teams', (req, res) => {
+    mongoose.model('Team').aggregate([{
+        $group : {_id: '_id', teams : {$sum : 1}}
+    }], (err, result) => {
+        if (err){
+            return Response.selectError(res, err);
+        }
+
+        Response.success(res, 'Statistics of teams', {
+            teams: result[0].teams
+        });
+    });
+});
+
 module.exports = router;
