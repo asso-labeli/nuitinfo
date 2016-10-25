6c892a03dba9b4607422abaa4e64d2df09e11ec8
HackJack <jack03113@gmail.com>
2016-10-25 17:05:49 +0200

    Update Register form

--- app/src/pages/Register.vue
+++ app/src/pages/Register.vue
@@ -4,7 +4,7 @@
         <form v-on:submit.prevent="register" class="userForm">
             <input type="hidden" name="post" value="post"/>
 
-            <h2 class="title" style="padding-top: 0">Informations personnelles</h2>
+            <h2 class="title" style="padding-top: 0">Informations de connexion</h2>
             <div class="formField">
                 <div class="labelWrapper">
                     <label for="email"><span class="special">Email :</span></label>
@@ -13,6 +13,17 @@
                     <input id="email" type="email" v-model="user.email" required="required"/>
                 </div>
             </div>
+
+            <div class="formField">
+                <div class="labelWrapper">
+                    <label for="email"><span class="special">Mot de passe :</span></label>
+                </div>
+                <div class="contentWrapper">
+                    <input id="email" type="email" v-model="user.password" required="required"/>
+                </div>
+            </div>
+
+            <h2 class="title" style="padding-top: 0">Informations personnelles</h2>
 
             <div class="formField">
                 <div class="labelWrapper">
@@ -37,8 +48,7 @@
                     <label for="birthday"><span class="special">Date de naissance :</span></label>
                 </div>
                 <div class="contentWrapper">
-                    <input id="birthday" type="date" v-model="user.birthday" required="required"
-                           placeholder="dd/mm/aaaa"
+                    <input id="birthday" type="date" v-model="user.birthday" placeholder="dd/mm/aaaa"
                            pattern="(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19|20)\d\d"/>
                 </div>
             </div>
@@ -53,51 +63,56 @@
             </div>
 
             <h2 class="title">Logistique</h2>
-            <div class="radioField">
-                <div class="labelWrapper">
-                    <label>Apporteras-tu <b>ton propre matériel</b> ? : </label>
+            <div class="checkbox-line">
+                <label for="hasMaterial">Comptes-tu apporter <b>ton propre matériel</b> : </label>
+                <input type="checkbox" id="hasMaterial" v-model="user.material.hasMaterial"/>
+            </div>
+
+            <div class="checkbox-line">
+                <label for="isDesktop">Est-ce que tu apporteras <b>un ordinateur fixe</b> : </label>
+                <input type="checkbox" id="isDesktop" v-model="user.material.isDesktop"/>
+            </div>
+
+            <div class="checkbox-line">
+                <label for="hasWiFi">Est ce que ton matériel est équipé du <b>WiFi</b> : </label>
+                <input type="checkbox" id="hasWiFi" v-model="user.material.hasWiFi"/>
+            </div>
+
+
+            <h2 class="title">Compte CREMI</h2>
+            <div class="checkbox-line">
+                <label for="isDesktop">Possèdes-tu un compte au CREMI : </label>
+                <input type="checkbox" id="isDesktop" v-model="user.cremiAccount.needed"/>
+            </div>
+
+            <div v-if="!user.cremiAccount.needed">
+                <div class="formField">
+                    <div class="labelWrapper">
+                        <label for="studentNumber"><span class="special">Numéro étudiant :</span></label>
+                    </div>
+                    <div class="contentWrapper">
+                        <input id="studentNumber" type="text" v-model="user.cremiAccount.studentNumber"/>
+                    </div>
                 </div>
-                <div class="radioWrapper">
-                    <input id="noHardware" type="radio" name="hardware" value="0"/>
-                    <label for="noHardware">Non</label>
-                    <input id="hardware" type="radio" name="hardware" value="1" checked="checked"/>
-                    <label for="hardware">Oui</label>
+                <div class="formField">
+                    <div class="labelWrapper">
+                        <label for="studentMail"><span class="special">Adresse e-mail étudiante :</span></label>
+                    </div>
+                    <div class="contentWrapper">
+                        <input id="studentMail" type="text" v-model="user.cremiAccount.studentMail"/>
+                    </div>
+                </div>
+
+                <div class="checkbox-line">
+                    <label for="cremiCharter">Je déclare avoir pris connaissance de la charte d'utilisation du CREMI et
+                        de la suivre : </label>
+                    <input type="checkbox" id="cremiCharter" v-model="user.cremiAccount.charter"/>
                 </div>
             </div>
 
-            <div class="radioField">
-                <div class="labelWrapper">
-                    <label>Quel <b>type</b> de matériel apporteras-tu ? : </label>
-                </div>
-                <div class="radioWrapper">
-                    <input id="laptop" type="radio" name="hardwareType" value="laptop" checked="checked"/>
-                    <label for="laptop">Ordinateur portable</label>
-                    <input id="UC" type="radio" name="hardwareType" value="UC"/>
-                    <label for="UC">Ordinateur de bureau</label>
-                </div>
-            </div>
+            <h2 class="title">Recrutement</h2>
 
-            <div class="radioField">
-                <div class="labelWrapper">
-                    <label>Est-il équipé du <b>WiFi</b> ? : </label>
-                </div>
-                <div class="radioWrapper">
-                    <input id="wifi" type="radio" name="wifi" value="1"/>
-                    <label for="wifi">Oui</label>
-                    <input id="noWifi" type="radio" name="wifi" value="0" checked="checked"/>
-                    <label for="noWifi">Non</label>
-                </div>
-            </div>
-
-            <h2 class="title">Engagements</h2>
-
-            <div style="display:flex;flex-direction: row;">
-                <label for="cremiCharter">Je déclare avoir pris connaissance de la charte d'utilisation du CREMI et de
-                    la suivre : </label>
-                <input type="checkbox" id="cremiCharter" v-model="user.cremiCharter"/>
-            </div>
-
-            <div style="display:flex;flex-direction: row;">
+            <div class="checkbox-line">
                 <label for="mailForRecruitment">J'accepte que mon e-mail soit communiqué à nos partenaires
                     <b>uniquement</b> à des fins de recrutement : </label>
                 <input type="checkbox" id="mailForRecruitment" v-model="user.mailForRecruitment"/>
@@ -124,12 +139,18 @@
                     firstName: 'John',
                     lastName: 'Snow',
                     birthday: '1970-01-01',
-                    school: '',
-                    material: true,
-                    isDesktop: false,
-                    hasWiFi: true,
+                    material: {
+                        hasMaterial: true,
+                        isDesktop: false,
+                        hasWiFi: true,
+                    },
+                    cremiAccount: {
+                        needed: false,
+                        studentNumber: null,
+                        studentMail: "",
+                        charter: false
+                    },
                     mailForRecruitment: true,
-                    cremiCharter: false
                 }
             };
         },
@@ -156,5 +177,10 @@
             max-width: 1200px;
             margin: 0 auto;
         }
+    }
+
+    .checkbox-line {
+        display: flex;
+        flex-direction: row;
     }
 </style>
