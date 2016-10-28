10b08fd8ac7e8e4911ae382195c64c901830c137
Jack3113 <jack03113@gmail.com>
2016-10-28 03:00:34 +0200

    Add cookie library Store token in cookie Update router

---
+++ app/src/libraries/cookie.js
@@ -0,0 +1,20 @@
+export function get(key) {
+    let name = key + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+export function set(key, value) {
+    let expires = new Date();
+    expires.setTime(expires.getTime() + (60 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + '; expires=' + expires.toUTCString() + ";path=/";
+}
