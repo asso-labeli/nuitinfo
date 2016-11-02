23dcbd4922cadc14e99fcd0a29721f58ae4a0614
aamulumi <florian@kauder.fr>
2016-11-02 12:41:09 +0100

    feat(server): Add GET /users/byInstitution

--- server/tools/routes/statistics.js
+++ server/tools/routes/statistics.js
@@ -2,6 +2,7 @@
 
 let express = require('express');
 let mongoose = require('mongoose');
+let async = require('async');
 
 let Response = require('../response');
 
@@ -38,6 +39,41 @@ router.get('/teams', (req, res) => {
 
         Response.success(res, 'Statistics of teams', {
             teams: result[0].teams
+        });
+    });
+});
+
+router.get('/users/byInstitution', (req, res) => {
+    mongoose.model('User').aggregate([{
+        $group: {_id: '$school.institution', users : {$sum : 1}}
+    }], (err, result) => {
+        if (err){
+            return Response.selectError(res, err);
+        }
+        if (!result || !result[0]){
+            return Response.resourceNotFound(res, 'users');
+        }
+
+        async.map(result, (oneResult, callback) => {
+            if (oneResult._id === null){
+                return callback(null, {
+                    institution: null,
+                    users: oneResult.users
+                });
+            }
+
+            mongoose.model('Institution').findById(oneResult._id, '-__v', (err, institution) => {
+                callback(err, {
+                    institution: institution,
+                    users: oneResult.users
+                });
+            });
+        }, (err, populatedResult) => {
+            if (err){
+                return Response.selectError(res, err);
+            }
+
+            Response.success(res, 'Statistics of users by institution', populatedResult);
         });
     });
 });
