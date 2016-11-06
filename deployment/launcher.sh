73707adef32b5d7be696869544003c554085b743
Jack3113 <jack03113@gmail.com>
2016-11-06 02:32:42 +0100

    Upgrade deployment

--- deployment/launcher.sh
+++ deployment/launcher.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env bash
 
-if [ ${1} -eq "generate" ]
+if [[ $# -eq 0 ]]
 then
-    echo "Dockerfile generation in progress"
+    echo "Usage"
+    echo "./launcher generate"
+    echo "./launcher build"
+    echo "./launcher launch"
 else
-    if [ ${1} -eq "build" ]
+    if [ ${1} -eq "generate" ]
     then
-        echo "Docker build in progress"
+        echo "Dockerfile generation in progress"
+        cp DockerTemplate Dockerfile
+        echo "You can build now"
     else
-        if [ ${1} -eq "launch" ]
+        if [ ${1} -eq "build" ]
         then
-            echo "Docker launch in progress"
+            echo "Docker build in progress"
+            docker build . -t nuitinfo
         else
-            echo "Usage"
+            if [ ${1} -eq "launch" ]
+            then
+                echo "Docker launch in progress"
+                docker rm -f ndi
+                if [ $# -eq 2 ]
+                then
+                    docker run -p 8080:${2} --name ndi -d nuitinfo
+                else
+                    docker run -p 8080:8080 --name ndi -d nuitinfo
+                fi
+            else
+                echo "Usage"
+                echo "./launcher generate"
+                echo "./launcher build"
+                echo "./launcher launch"
+            fi
         fi
     fi
 fi
