8b38e9d5d7f0bd8813a919f8be4844a947461e90
HackJack <jack03113@gmail.com>
2016-11-28 17:15:05 +0100

    Close route to create user Close route to edit user

--- server/entities/team/team.controller.js
+++ server/entities/team/team.controller.js
@@ -157,6 +157,9 @@ module.exports = function (teamSchema) {
     /* Express calls */
 
     teamSchema.statics.exCreate = function (req, res) {
+        Response.insertError(res, 'Inscriptions terminées');
+        return false;
+
         if (!req.isLogged()) {
             return Response.notAllowed(res);
         }
